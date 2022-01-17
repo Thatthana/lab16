41840100853ae0cbe91b2f61ea5c7f68179c50b6
Fluke <hunterx52110@gmail.com>
2022-01-17 21:39:04 +0700

    completed

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -6,6 +6,7 @@ using namespace std;
 void shuffle(int &,int &,int &,int &);
 
 int main(){
+	srand(time(0));
 	int a = 50, b = 100, c = 500, d = 1000;
 	
 	srand(time(0));	
@@ -17,3 +18,16 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int *money[] = {&a, &b, &c, &d};
+	int random1 = rand()%4,random2 = rand()%4;
+
+	while (random1 == random2){
+		random2 = rand()%4;
+	}
+		int temp;
+		temp = *money[random1];
+		*money[random1] = *money[random2];
+		*money[random2] = temp;
+	}
